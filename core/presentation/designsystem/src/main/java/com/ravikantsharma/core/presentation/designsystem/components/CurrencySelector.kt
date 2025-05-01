d24722766642cba957df4531bab37645d3de98f5
Ravikant <ravik6373@gmail.com>
2025-05-01 12:20:06 +0530

    Updated Currency Selector Component

--- core/presentation/designsystem/src/main/java/com/ravikantsharma/core/presentation/designsystem/components/CurrencySelector.kt
+++ core/presentation/designsystem/src/main/java/com/ravikantsharma/core/presentation/designsystem/components/CurrencySelector.kt
@@ -4,14 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -34,11 +40,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ravikantsharma.core.presentation.designsystem.ExpenseManagerTheme
 import com.ravikantsharma.core.presentation.designsystem.TickIcon
+import com.ravikantsharma.core.presentation.designsystem.primaryFixed
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,6 +54,9 @@ fun <T> CurrencySelector(
     modifier: Modifier = Modifier,
     title: String? = null,
     selectedOption: T,
+    fontStyle: TextStyle = MaterialTheme.typography.bodyMedium,
+    showIconBackground: Boolean = false,
+    showMenuIcon: Boolean = true,
     options: Array<T>,
     currencyDisplay: (T) -> String,
     currencyTitleDisplay: (T) -> String,
@@ -68,6 +79,8 @@ fun <T> CurrencySelector(
         ) {
             CurrencySelectorTextField(
                 expanded = expanded,
+                fontStyle = fontStyle,
+                showIconBackground = showIconBackground,
                 selectedOption = selectedOption,
                 currencyDisplay = currencyDisplay,
                 currencyTitleDisplay = currencyTitleDisplay,
@@ -77,23 +90,30 @@ fun <T> CurrencySelector(
             ExposedDropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
-                modifier = Modifier.background(
-                    color = MaterialTheme.colorScheme.surfaceContainerLowest
-                )
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
             ) {
                 options.forEach { option ->
                     DropdownMenuItem(
+                        modifier = Modifier.height(48.dp), // Match height with selector
                         text = {
                             CurrencyRow(
+                                fontStyle = fontStyle,
+                                showIconBackground = showIconBackground,
                                 currencyCode = currencyDisplay(option),
                                 currencyName = currencyTitleDisplay(option),
+                                showMenuIcon = showMenuIcon,
                                 showTickIcon = option == selectedOption
                             )
                         },
                         onClick = {
                             onItemSelected(option)
                             expanded = false
-                        }
+                        },
+                        contentPadding = PaddingValues(
+                            vertical = 0.dp,
+                            horizontal = 0.dp
+                        )
                     )
                 }
             }
@@ -105,6 +125,8 @@ fun <T> CurrencySelector(
 @Composable
 private fun <T> ExposedDropdownMenuBoxScope.CurrencySelectorTextField(
     expanded: Boolean,
+    fontStyle: TextStyle,
+    showIconBackground: Boolean,
     selectedOption: T,
     currencyDisplay: (T) -> String,
     currencyTitleDisplay: (T) -> String,
@@ -112,19 +134,11 @@ private fun <T> ExposedDropdownMenuBoxScope.CurrencySelectorTextField(
 ) {
     val interactionSource = remember { MutableInteractionSource() }
 
-    TextField(
+    BasicTextField(
         value = "",
         onValueChange = {},
         readOnly = true,
-        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-            unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        ),
+        interactionSource = interactionSource,
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp)
@@ -133,36 +147,80 @@ private fun <T> ExposedDropdownMenuBoxScope.CurrencySelectorTextField(
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.surfaceContainerLowest)
             .clickable { onExpandedChange(!expanded) },
-        singleLine = true,
-        interactionSource = interactionSource,
-        placeholder = {
-            CurrencyRow(
-                currencyCode = currencyDisplay(selectedOption),
-                currencyName = currencyTitleDisplay(selectedOption)
-            )
+        decorationBox = @Composable {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                CurrencyRow(
+                    showIconBackground = showIconBackground,
+                    currencyCode = currencyDisplay(selectedOption),
+                    currencyName = currencyTitleDisplay(selectedOption),
+                    fontStyle = fontStyle,
+                    showMenuIcon = true,
+                    modifier = Modifier.weight(1f)
+                )
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded,
+                    modifier = Modifier.padding(end = 12.dp)
+                )
+            }
         }
     )
 }
 
 @Composable
 private fun CurrencyRow(
+    showIconBackground: Boolean,
     currencyCode: String,
     currencyName: String,
+    fontStyle: TextStyle,
+    showMenuIcon: Boolean,
     showTickIcon: Boolean = false,
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(end = 12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
+
+        if (showIconBackground) {
+            Spacer(modifier = Modifier.padding(start = 4.dp))
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = if (showMenuIcon) {
+                            primaryFixed
+                        } else {
+                            Color.Transparent
+                        },
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .size(40.dp)
+            ) {
+                if (showMenuIcon) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = currencyCode,
+                        style = MaterialTheme.typography.labelMedium,
+                    )
+                }
+            }
+        } else {
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Text(
+                text = currencyCode,
+                style = MaterialTheme.typography.labelMedium,
+            )
+        }
+
         Text(
-            text = currencyCode,
-            style = MaterialTheme.typography.labelMedium
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
+            modifier = Modifier.padding(start = 8.dp),
             text = currencyName,
-            style = MaterialTheme.typography.bodyMedium,
+            style = fontStyle,
             textAlign = TextAlign.End
         )
         if (showTickIcon) {
@@ -183,17 +241,46 @@ fun PreviewCurrencySelector() {
         Surface(
             color = MaterialTheme.colorScheme.background
         ) {
-            CurrencySelector(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                title = "Currency",
-                selectedOption = FakeCurrency.INR,
-                options = FakeCurrency.entries.toTypedArray(),
-                currencyDisplay = { it.symbol },
-                currencyTitleDisplay = { it.title },
-                onItemSelected = {}
-            )
+            Column {
+                CurrencySelector(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    title = "Currency",
+                    selectedOption = FakeCurrency.INR,
+                    options = FakeCurrency.entries.toTypedArray(),
+                    currencyDisplay = { it.symbol },
+                    currencyTitleDisplay = { it.title },
+                    onItemSelected = {}
+                )
+
+                CurrencySelector(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    showIconBackground = true,
+                    fontStyle = MaterialTheme.typography.labelMedium,
+                    selectedOption = Expenses.HOME,
+                    options = Expenses.entries.toTypedArray(),
+                    currencyDisplay = { it.symbol },
+                    currencyTitleDisplay = { it.title },
+                    onItemSelected = {}
+                )
+
+                CurrencySelector(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    showIconBackground = true,
+                    fontStyle = MaterialTheme.typography.labelMedium,
+                    selectedOption = Recurring.ONE_TIME,
+                    showMenuIcon = false,
+                    options = Recurring.entries.toTypedArray(),
+                    currencyDisplay = { it.symbol },
+                    currencyTitleDisplay = { it.title },
+                    onItemSelected = {}
+                )
+            }
         }
     }
 }
@@ -202,4 +289,26 @@ enum class FakeCurrency(val symbol: String, val title: String) {
     INR("₹", "Indian Rupee"),
     USD("$", "US Dollar"),
     EUR("€", "Euro")
+}
+
+enum class Expenses(val symbol: String, val title: String) {
+    HOME("\uD83C\uDFE0", "Home"),
+    FOOD("\uD83C\uDF55", "Food & Groceries"),
+    ENTERTAINMENT("\uD83D\uDCBB", "Entertainment"),
+    CLOTHING("\uD83D\uDC54", "Clothing & Accessories"),
+    HEALTH("❤\uFE0F", "Health & Wellness"),
+    PERSONAL_CARE("\uD83D\uDEC1", "Personal Care"),
+    TRANSPORTATION("\uD83D\uDE97", "Transportation"),
+    EDUCATION("\uD83C\uDF93", "Education"),
+    SAVINGS("\uD83D\uDC8E", "Saving & Investments"),
+    OTHER("⚙\uFE0F", "Other")
+}
+
+enum class Recurring(val symbol: String, val title: String) {
+    ONE_TIME("\uD83D\uDD04", "Does not repeat"),
+    DAILY("\uD83D\uDD04", "Daily"),
+    WEEKLY("\uD83D\uDD04", "Weekly"),
+    WEEKLY_ON_THIS_DAY("\uD83D\uDD04", "Weekly on Monday"),
+    MONTHLY_ON_THIS_DAY("\uD83D\uDD04", "Monthly on the 14th"),
+    YEARLY_ON_THIS_DAY("\uD83D\uDD04", "Yearly on Feb 14th")
 }
