e2eae924a8693d69acdaa7cd71ebc0c79ba0bf66
Ravikant <ravik6373@gmail.com>
2025-05-05 13:04:11 +0530

    minor changes

--- auth/data/src/main/java/com/ravikantsharma/auth/data/di/AuthDataModule.kt
+++ auth/data/src/main/java/com/ravikantsharma/auth/data/di/AuthDataModule.kt
@@ -1,10 +1,8 @@
 package com.ravikantsharma.auth.data.di
 
-import com.ravikantsharma.core.domain.auth.repository.UserInfoRepository
 import com.ravikantsharma.auth.domain.usecase.DecryptPinUseCase
 import com.ravikantsharma.auth.domain.usecase.EncryptPinUseCase
 import com.ravikantsharma.auth.domain.usecase.EncryptionUseCases
-import com.ravikantsharma.auth.domain.usecase.FormatExampleUseCase
 import com.ravikantsharma.auth.domain.usecase.InitiateLoginUseCase
 import com.ravikantsharma.auth.domain.usecase.IsUserNameDuplicateUseCase
 import com.ravikantsharma.auth.domain.usecase.IsUsernameValidUseCase
@@ -14,8 +12,6 @@ import com.ravikantsharma.auth.domain.usecase.RegisterUseCases
 import com.ravikantsharma.auth.domain.usecase.RegisterUserUseCase
 import com.ravikantsharma.auth.domain.usecase.ValidateSelectedPreferences
 import com.ravikantsharma.core.domain.preference.usecase.SetPreferencesUseCase
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val authDataModule = module {
